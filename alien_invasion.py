7155c29d93c51c3832451cf7d11dd42af8954634
jerenteria <renteria.juan96@gmail.com>
2022-11-30 20:31:19 -0800

    refactored run_game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     # constructor class
@@ -16,6 +17,11 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
 
+        # Create an instance(object of the class Ship) of Ship after the screen has been created
+        # argument self refers to current instance of AlienInvasion; gives Ship access to games resources, such as the screen object
+        # We assign this ship instance to self.ship 
+        self.ship = Ship(self)
+
         # set background color
         self.bg_color = (230, 230, 230)
     
@@ -30,10 +36,15 @@ class AlienInvasion:
                     # call sys.exit() to exit the game
                     sys.exit()
 
+                # The surface(part of the screen where a game element can be displayed)
+                    # Each element in the game(ship/alien) is its own surface
+                    # Screen itself is a surface
                 # Redraw the screen during each pass through the loop
                 # Fills screen with background color
                 self.screen.fill(self.settings.bg_color)
-
+                # Call ship.blitme() after filling the screen so the ship appears on top of the background
+                self.ship.blitme()
+                
                 # Make the most recently drawn screen visible
                 # pygame.display.flip() continually updates display to show the new positions of game elements and hides the old ones
                 # creating the illusion of smooth movements 
--- ship.py
+++ ship.py
@@ -3,13 +3,21 @@ import pygame
 class Ship:
     """A class to manage the ship """
 
+    # Giving ai_game is a reference to the current instance of the AlienInvasion class(will give class Ship access to all the game
+    # resources defined in AlienInvasion class)
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
+        # Assign screen to an attribute of Ship to access it easily in all the methods in this class
         self.screen = ai_game.screen
+        # Access the screens rect(rectangle) attribute using the get_rect() method and assign it to self.screen_rect
+            # this allows us to place the ship in the correct location on the screen 
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship image and get its rect
+        # Load the ship image by giving the location of image
+            # This returns a surface(the ship) which we assigned to self.image
         self.image = pygame.image.load('images/ship.bmp')
+        # After image is loaded we call self.rect() to access the ships surface's rect attribute so we can later use it to 
+        # place the ship
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom of center of the screen
