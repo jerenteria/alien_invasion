6b27f9fc977527ae7cf3c8f39351abcc9dd630de
jerenteria <renteria.juan96@gmail.com>
2022-12-06 20:56:29 -0800

    added more aliens to the fleet and refactored some methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -105,8 +105,40 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
-        # Make an alien
+        # Create an alien first before we perform calculations 
         alien = Alien(self)
+        # Get width/height of alien by creating tuple(used to store multiple items in a single variable) .size is the tuple
+        alien_width, alien_height = alien.rect.size
+        # Calculate the horizontal space available for aliens and the number of aliens that can fit into that space
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Determine the number of rows of aliens that fit on the screen
+        ship_height = self.ship.rect.height
+        # find available vertical space by subtracting the alien height from the top, ship height from the bottom, and two alien
+        # heights from the bottom of the screen
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Create the full fleet of aliens
+        # The outter loop counts from 0 to the number of rows we want
+        for row_number in range(number_rows):
+            # Loop that counts from 0 to the number of aliens we need to make
+            # Inner loop creates aliens for one row
+            for alien_number in range(number_aliens_x):
+                # Include argument for row number so each row can be further down the screen
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Create alien and place it in the row"""
+        # Create an alien 
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        # Set aliens x coordinate val to place it in the row
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        # Change aliens y-coordinate when its not in the first row to create empty space at top of screen
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
